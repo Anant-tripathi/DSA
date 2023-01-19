f8831324e3a8341b42b8639ebeb348dae71cef19
Anant Tripathi <anantraj.tripathi79@gmail.com>
2023-01-19 14:22:05 +0530

    binary search implementation in recursive way try 2

--- Searching/binarySearch_Recursive.js
+++ Searching/binarySearch_Recursive.js
@@ -4,19 +4,21 @@ function binarySearch_Recursive(arr, x, low, high)
 		return -1;
 	else
 	{
-		let mid = (low + (Math.floor(high-low)/2));
-		if(x == arr[mid])
+		let mid = (low + (Math.floor(high - low)/2));
+		if(arr[mid] == x)
 			return mid;
 		else
+		{
 			if(x > mid)
 				binarySearch_Recursive(arr, x, mid+1, high);
 			else
 				binarySearch_Recursive(arr, x, low, mid-1);
+		}
 	}
 }
 
 let arr = [10,20,30,40,50,60,70];
-let x = 40;
+let x = 70;
 let res = binarySearch_Recursive(arr, x, 0, arr.length);
 
 (res == -1) ? console.log("Element not found") :
