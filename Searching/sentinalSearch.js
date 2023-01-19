eb1ad61f8e634de2c60aaa68bbc5699034308e82
Anant Tripathi <anantraj.tripathi79@gmail.com>
2023-01-19 11:17:08 +0530

    sentinalSearch repeat 2nd time

--- Searching/sentinalSearch.js
+++ Searching/sentinalSearch.js
@@ -1,1 +1,19 @@
-function  
+function sentinalSearch(arr, n, key)
+{
+	const last = arr[n-1];
+	arr[n-1] = key;
+	let i=0;
+	while(arr[i] != key)
+		i++;
+	arr[n-1] = last;
+	if((i<n-1) || (arr[n-1] === key))
+		console.log(key + " is found at index " + i);
+	else
+		console.log("Element not found");
+}
+
+let arr = [10,20,180,58,058,4,2,045,9756,0058];
+let key = 0058;
+let n = arr.length;
+
+sentinalSearch(arr, n, key);
