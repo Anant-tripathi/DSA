e8234e2709057c76da5c09f3157be39ea94ed3c3
Anant Tripathi <anantraj.tripathi79@gmail.com>
2023-01-19 14:09:03 +0530

    binary search implementation in iterative way try 3

--- Searching/binarySearch_Iterative.js
+++ Searching/binarySearch_Iterative.js
@@ -1,30 +1,27 @@
 function binarySearch_Iterative(arr, x)
 {
 	let n = arr.length;
-	let mid;
 	let low = 0;
 	let high = n-1;
+	let mid;
 
 	while(low < high)
 	{
-		mid = low + Math.floor((high - low)/2);
-
+		mid = low + (Math.floor(high - low)/2);
 		if(arr[mid] == x)
 			return mid;
 		else
-			if(x > arr[mid])
-				low = mid + 1;
+			if(x > mid)
+				low = mid+1;
 		else
-			high = mid - 1;
+			high = mid-1;
 	}
 	return -1;
 }
 
 let arr = [10,20,30,40,50,60,70];
-let x = 40;
-
+let x = 60;
 let res = binarySearch_Iterative(arr, x);
-if(res == -1)
-	console.log("Element not found");
-else
+
+(res == -1) ? console.log("Element not found") : 
 	console.log(x + " found at index " + res);
