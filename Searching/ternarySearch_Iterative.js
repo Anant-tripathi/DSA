1559814855fd1f5416478a377029a946818c4860
Anant Tripathi <anantraj.tripathi79@gmail.com>
2023-01-19 21:05:17 +0530

    ternary search implementation using iteration try 2

--- Searching/ternarySearch_Iterative.js
+++ Searching/ternarySearch_Iterative.js
@@ -1,25 +1,24 @@
-function ternarySearch(arr, x)
+function ternarySearch(arr, key)
 {
 	let n = arr.length;
 	let low = 0;
 	let high = n-1;
-	let mid1, mid2;
 
 	while(low < high)
 	{
-		mid1 = low + Math.floor((high - low)/3);
-		mid2 = high - Math.floor((high - low)/3);
+		let mid1 = low + Math.floor((high - low)/3);
+		let mid2 = high - Math.floor((high - low)/3);
 
-		if(arr[mid1] == x)
+		if(arr[mid1] == key)
 			return mid1;
-		if(arr[mid2] == x)
+		if(arr[mid2] == key)
 			return mid2;
 
-		if(x < arr[mid1])
-			high = mid1-1;
+		if(key > arr[mid1])
+			low = mid1+1;
 		else
-			if(x > arr[mid2])
-				low = mid2+1;
+			if(key < arr[mid2])
+				high = mid2-1;
 		else
 		{
 			low = mid1+1;
@@ -27,10 +26,10 @@ function ternarySearch(arr, x)
 		}
 	}
 	return -1;
-}
+};
 
 let arr = [10,20,30,40,50,60,70]
-let x = 83;
+let x = 30;
 
 let res = ternarySearch(arr, x);
 (res == -1) ? console.log("Element not found") :
