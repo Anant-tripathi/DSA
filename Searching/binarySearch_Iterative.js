f0ccbd88ee84a2edfa838a13c51d0258ef6bae6e
Anant Tripathi <anantraj.tripathi79@gmail.com>
2023-01-19 22:02:08 +0530

    corrected the comparision for while loop condition, it is 'less than equal to'

--- Searching/binarySearch_Iterative.js
+++ Searching/binarySearch_Iterative.js
@@ -1,23 +1,24 @@
-function binarySearch_Iterative(arr, x)
+function binarySearch_Iterative(arr, key)
 {
 	let n = arr.length;
 	let low = 0;
 	let high = n-1;
-	let mid;
 
-	while(low < high)
+	while(low <= high)
 	{
-		mid = low + (Math.floor(high - low)/2);
-		if(arr[mid] == x)
+		let mid = low + Math.floor((high - low)/2);
+
+		if(arr[mid] == key)
 			return mid;
+
 		else
-			if(x > mid)
-				low = mid+1;
+			if(key > arr[mid])
+			low = mid+1;
 		else
 			high = mid-1;
 	}
 	return -1;
-}
+};
 
 let arr = [10,20,30,40,50,60,70];
 let x = 60;
